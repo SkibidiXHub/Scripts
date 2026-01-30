07f40e262ce5542ab6abb16e44192f1ba171ddb3
SkibidiXHub <buinguyenphuc0912@gmail.com>
2026-01-30 17:41:11 +0700

    Add files via upload

--- redzfakenotreal.lua
+++ redzfakenotreal.lua
@@ -1,8 +1,8 @@
 ```local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/daucobonhi/UiRedzV5/refs/heads/main/DemoUi.lua"))();
 
    local Windows = redzlib:MakeWindow({
-    Title = "Anura Gaming",
-    SubTitle = "Youtuber Top 1",
+    Title = "Sigma Hub",
+    SubTitle = "Jack",
     SaveFolder = "Blox Fruits.lua"
 })
 Windows:AddMinimizeButton({
@@ -17,22 +17,18 @@ Farm:AddSection("Main Farm")
     Farm:AddToogle({
     Name = "Auto Cày Cấp",
     Callback = function()
-    end
-})
+    end})
+
     Farm:AddToogle({
     Name = "Auto Quái Gần",
     Callback = function()
-    end
-})
+    end}}
+
     Farm:AddToogle({
     Name = "Auto Nhà Máy",
     Callback = function()
-    end
-})
+    end})
 
-Farm:AddSection("Cặc Cha")
-    Farm:AddToogle({
-    Name = "Bú Cu Ronaldo",
-    Callback = function()
-    end
-})
+
+
+
